ebd3e89ecc7024dd5b1b89e584dfcf42103e5c00
Uriolivei7 <217280383+Uriolivei7@users.noreply.github.com>
2026-01-01 16:43:55 -0500

    Pelispedia activado

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,9 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2")
         // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT") {
+            exclude(group = "com.github.vidstige", module = "jadb")
+        }
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
